a0b64161f41e26d2dc3b01a71479b86ce007021e
dltmddus1998 <dltmddus1998@naver.com>
2022-06-07 20:05:19 +0900

    ADD: DI Refactoring🏭

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,28 @@
+import { Component } from './components/component.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { PageComponent, Composable, PageItemComponent } from './components/page/page.js';
 
 class App {
-    private readonly page: PageComponent;
+    // Component이면서 addChild를 할 수 있는 Composable이 가능한 요소
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
-        this.page = new PageComponent();
+        this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
 
         const video = new VideoComponent('Video Title', 'https://www.youtube.com/embed/yA4d5ZydVVQ');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
 
         const note = new NoteComponent('Note Title', 'Note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const todo = new TodoComponent('Todo Title', 'Todo Item');
-        todo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(todo);
     }
 }
 

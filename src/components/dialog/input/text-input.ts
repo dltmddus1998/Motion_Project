60cd4b4e474892d02f74424ca63659431a4f437f
이승연 <sylee1274@mz.co.kr>
2023-02-02 16:51:50 +0900

    Feat: dialog add - default version

--- src/components/dialog/input/text-input.ts
+++ src/components/dialog/input/text-input.ts
@@ -1,15 +1,16 @@
+import { TextData } from './../dialog.js';
 import { BaseComponent } from './../../component.js';
-export class TextSectionInput extends BaseComponent<HTMLElement> {
+export class TextSectionInput extends BaseComponent<HTMLElement> implements TextData {
   constructor() {
     super(`<div>
-          <div class="form__container">
-            <label for="title">Title</label>
-            <input type="text" id="title" />
-          </div>
-          <div class="form__container">
-            <label for="url">Body</label>
-            <textarea type="text" row="3" id="body"></textarea>
-          </div>
+            <div class="form__container">
+                <label for="title">Title</label>
+                <input type="text" id="title" />
+            </div>
+            <div class="form__container">
+                <label for="body">Body</label>
+                <textarea type="text" row="3" id="body"></textarea>
+            </div>
         </div>`);
   }
 

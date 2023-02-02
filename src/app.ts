4c246d027a82a73328618bd0668903d2a694c005
이승연 <sylee1274@mz.co.kr>
2023-02-02 16:02:56 +0900

    Feat: dialog empty

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,44 @@
-import { Component } from './components/component.js';
+import { InputDialog } from './components/dialog/dialog.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent, Composable, PageItemComponent } from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
+import { Component } from './components/component.js';
 
 class App {
-    // Component이면서 addChild를 할 수 있는 Composable이 가능한 요소
-    private readonly page: Component & Composable;
-    constructor(appRoot: HTMLElement) {
-        this.page = new PageComponent(PageItemComponent);
-        this.page.attachTo(appRoot);
+  private readonly page: Component & Composable;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent(PageItemComponent);
+    this.page.attachTo(appRoot);
 
-        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        this.page.addChild(image);
+    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+    this.page.addChild(image);
 
-        const video = new VideoComponent('Video Title', 'https://www.youtube.com/embed/yA4d5ZydVVQ');
-        this.page.addChild(video);
+    const video = new VideoComponent('Video Title', 'https://youtu.be/K3-jG52XwuQ');
+    this.page.addChild(video);
 
-        const note = new NoteComponent('Note Title', 'Note Body');
-        this.page.addChild(note);
+    const note = new NoteComponent('Note Title', 'Note Body');
+    this.page.addChild(note);
 
-        const todo = new TodoComponent('Todo Title', 'Todo Item');
-        this.page.addChild(todo);
-    }
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    this.page.addChild(todo);
+
+    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+    imageBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+
+      dialog.setOnCloseListenr(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListenr(() => {
+        // 섹션을 만들어서 페이지에 추가 해준다
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.attachTo(document.body);
+    });
+  }
 }
 
-// 동적으로 만드는게 아니라 개발시 정확히 정해진 경우 -> 무조건 null 아니고 HTMLElement 타입이라고 Type Assertion로 표시
-new App(document.querySelector('.document')! as HTMLElement)
+new App(document.querySelector('.document')! as HTMLElement);
--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -1,33 +1,32 @@
 import { BaseComponent } from './../../component.js';
 export class VideoComponent extends BaseComponent<HTMLElement> {
-    constructor(title: string, url: string) {
-        super(`<section class="video">
-                <div class="video__player"><iframe class="video__iframe"></iframe></div>
-                <h3 class="video__title"></h3>
-            </section>`);
-        
-        const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-        // url에서 video_ID만 추출하는 함수를 만들어보자.
-        iframe.src = this.convertToEmbeddedURL(url);
-        
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="page-item__title video__title"></h3>
+        </section>`);
 
-        const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
-        titleElement.textContent = title;
-    } 
-    
+    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+    iframe.src = this.convertToEmbeddedURL(url);
 
-    private convertToEmbeddedURL(url: string): string {
-        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9]{11}))|(?:youtu.be\/([a-zA-Z0-9]{11})))/;
-        const match = url.match(regExp);
-        console.log(match);
-        
+    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
 
-        const videoId = match? match[1] || match[2] : undefined;
-        if (videoId) {
-            return `https://www.youtube.com/embed/${videoId}`;
-        }
-        return url;
+  // input
+  // https://www.youtube.com/watch?v=K3-jG52XwuQ
+  // https://youtu.be/K3-jG52XwuQ
+  // output
+  // https://www.youtube.com/embed/K3-jG52XwuQ
+  // 정규표현식 Regex
+  // https://regexr.com/5l6nr
+  private convertToEmbeddedURL(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
     }
+    return url;
+  }
 }
-
-// <iframe width="950" height="534" src="https://www.youtube.com/embed/yA4d5ZydVVQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
